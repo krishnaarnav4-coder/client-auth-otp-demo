67373b99470ac0f9cd46e355714107b29acd5544
vixalabs <krishnaarnav4@gmail.com>
2026-02-21 23:56:17 +0530

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,12 @@
+{
+  "name": "client-auth-otp-demo",
+  "version": "1.0.0",
+  "description": "Simple OTP authentication demo",
+  "main": "server/index.js",
+  "scripts": {
+    "start": "node server/index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
